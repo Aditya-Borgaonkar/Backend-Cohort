56047d126b9201984e6e14ca15e7adab12ef6fd4
Aditya-Borgaonkar <adityaborgaonkar4@gmail.com>
2026-01-26 18:46:46 +0530

    first commit

---
+++ Day-2/Server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+
+const app = express();//server instance create karana
+
+app.get('/', (req, res) => {
+    res.send("Hello from backend")
+})
+
+app.get('/about', (req, res) => {
+    res.send("This is about page")
+})
+
+
+app.get('/')
+app.listen(3000)/// server start karana
